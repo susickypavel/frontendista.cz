3a77c51a68265da5258c379a484d537ffb5b5378
Pavel Sušický <pavels.sus@gmail.com>
2020-02-19 14:47:30 +0100

    chore: test environment setup

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  verbose: true,
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  transform: {
+    "^.+\\.tsx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "~(.*)$": "<rootDir>/src$1",
+  },
+  testPathIgnorePatterns: ["./.next/", "./node_modules/"],
+}
---
+++ src/components/navigation/navigation.test.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { render } from "@testing-library/react"
+
+import Navigation from "./navigation.component"
+
+const useRouter = jest.spyOn(require("next/router"), "useRouter")
+
+function mockRouter(pathname: string) {
+  useRouter.mockImplementationOnce(() => ({
+    pathname,
+  }))
+}
+
+describe("Navigation", () => {
+  it("should render link ", () => {
+    mockRouter("/")
+
+    render(<Navigation />)
+  })
+})
