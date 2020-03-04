f44c85890fb7f0cd9872880dbb9397b40999ab48
Pavel Sušický <pavels.sus@gmail.com>
2020-03-04 21:33:31 +0100

    chore: webpack bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const path = require("path")
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
 
-module.exports = {
+module.exports = withBundleAnalyzer({
   webpack: config => {
     config.resolve.alias["~"] = path.resolve(__dirname, "src")
     return config
   },
-}
+})
