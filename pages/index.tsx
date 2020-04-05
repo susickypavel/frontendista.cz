351a15e46f7f574d8736f0c9d19544744dc22379
Pavel Sušický <pavels.sus@gmail.com>
2020-04-05 14:49:32 +0200

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,11 @@
 import React from "react";
 
-import { NextPage, GetStaticProps } from "next";
+import { NextPage } from "next";
 import { useRouter } from "next/router";
 
-import Logo from "~/components/index-page/logo/logo.component";
 import Seo from "~/components/page-layout/seo/seo.component";
-import Description from "~/components/index-page/description/description.component";
-import DescriptionSplitter from "~/components/index-page/description-splitter";
 
-import {
-  LandingBlock,
-  ContentBox,
-  PageHolder,
-  LandingBlockLeft,
-  LandingBlockRight,
-} from "~/components/index-page/index-page.styles";
+import { LandingBlock, PageHolder } from "~/components/index-page/index-page.styles";
 import BlogPostList, {
   PostPreview,
 } from "~/components/index-page/blog-post-list/blog-post-list.component";
@@ -32,15 +23,7 @@ const IndexPage: NextPage<Props> = ({ postPreviews }) => {
       <Seo pathname={pathname} />
       <LandingBlock>
         <LogoV2 />
-        {/* <LandingBlockLeft>
-          <Logo />
-        </LandingBlockLeft>
-        <DescriptionSplitter />
-        <LandingBlockRight>
-          <Description />
-        </LandingBlockRight> */}
       </LandingBlock>
-      {/* <ContentBox><BlogPostList postPreviews={postPreviews} /></ContentBox> */}
     </PageHolder>
   );
 };
--- src/components/index-page/index-page.styles.ts
+++ src/components/index-page/index-page.styles.ts
@@ -17,28 +17,3 @@ export const LandingBlock = styled.div`
     flex-direction: column;
   }
 `;
-
-export const ContentBox = styled.div`
-  min-height: 100vh;
-`;
-
-export const LandingBlockLeft = styled.div`
-  min-height: 100vh;
-  flex: 1 0 40%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-export const LandingBlockRight = styled.div`
-  min-height: 100vh;
-  flex: 1 0 40%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 32px 16px;
-  box-sizing: border-box;
-
-  @media (max-width: 550px) {
-    align-self: flex-start;
-  }
-`;
