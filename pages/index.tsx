8c01a1e686ba4623a2e61297020b0cde98e2759d
Pavel Sušický <pavels.sus@gmail.com>
2020-07-25 21:42:22 +0200

    chore: add prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const IndexPage: React.FC = () => {
   return (
-  <div>
-    <h1>Hello World</h1>
-  </div>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   );
 };
 
