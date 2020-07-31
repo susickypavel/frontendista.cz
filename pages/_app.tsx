79620f80b4709addc4be2b3b11115038cb897a81
Pavel Sušický <pavels.sus@gmail.com>
2020-07-31 22:28:48 +0200

    feat: add global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 /**
  * This component initializes the pages
  */
-import React from "react";
+import React, { Fragment } from "react";
 import { AppProps } from "next/app";
 
+import { GlobalStyle } from "src/styles/global-css";
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 };
 
 export default MyApp;
