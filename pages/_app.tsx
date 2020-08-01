397c89a4c62d9d14c3dd94094b7a7aac2da9c82e
Pavel Sušický <pavels.sus@gmail.com>
2020-08-01 18:32:20 +0200

    feat: next-seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,31 @@
  * This component initializes the pages
  */
 import React, { Fragment } from "react";
+
 import { AppProps } from "next/app";
+import { useRouter } from "next/dist/client/router";
+import { DefaultSeo } from "next-seo";
 
 import { GlobalStyle } from "src/styles/global-css";
+import { CSSReset } from "src/styles/reset-css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const { asPath } = useRouter();
+
+  const protocol = process.env.NODE_ENV === "development" ? "http://" : "https://";
+  const canonical = protocol + process.env.VERCEL_URL + asPath;
+
   return (
     <Fragment>
       <GlobalStyle />
+      <CSSReset />
+      <DefaultSeo
+        canonical={canonical}
+        twitter={{
+          handle: "@thesoreon",
+          cardType: "summary",
+        }}
+      />
       <Component {...pageProps} />
     </Fragment>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
+import PageLayout from "src/components/page-layout/page-layout.component";
+
 const IndexPage: React.FC = () => {
   return (
-    <div>
+    <PageLayout>
       <h1>This is heading</h1>
       <p>This is REGULAR paragraph</p>
       <p>This is BLACK paragraph</p>
       <p style={{ fontStyle: "ITALIC" }}>This is ITALIC paragraph</p>
-    </div>
+    </PageLayout>
   );
 };
 
