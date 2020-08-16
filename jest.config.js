80fe625c7d9073b533fbba9011fa13483448c00b
Pavel Sušický <pavels.sus@gmail.com>
2020-08-16 08:55:30 +0200

    change jest runner to ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
+/** @typedef {import('ts-jest')} */
+/** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
+  preset: "ts-jest",
   setupFilesAfterEnv: ["./test/setup.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/cypress/"],
+  moduleDirectories: ["node_modules", "./"],
+  globals: {
+    "ts-jest": {
+      tsConfig: "./test/tsconfig.spec.json",
+    },
+  },
 };
