2b6732dc6d72ce360e4ae88efd8540fea23b3408
Pavel Sušický <pavels.sus@gmail.com>
2020-08-18 21:28:47 +0200

    chore: add support for inlining svgs

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module "*.svg" {
+  const testing: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+
+  export default testing;
+}
