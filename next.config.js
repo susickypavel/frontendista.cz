0463f2d11a7b19e9d3f675d43b99b46c55c21ea5
Pavel Sušický <pavels.sus@gmail.com>
2020-10-31 20:54:46 +0100

    chore: implement next-compose-plugins

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+const withPlugins = require("next-compose-plugins");
+
+module.exports = withPlugins([], {
   env: {
     VERCEL_URL: process.env.VERCEL_URL,
   },
-};
+  webpack: config => {
+    return config;
+  },
+});
