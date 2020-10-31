298ef3fa3c467db5d6e3d61b8a0e4af7750c64db
Pavel Sušický <pavels.sus@gmail.com>
2020-11-01 00:48:32 +0100

    feat: init

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import type { NextPage } from "next"
+
+interface IndexProps {
+}
+
+const Index: NextPage<IndexProps> = () => {
+    return (
+        <div>Hello, World</div>
+    )
+}
+
+export default Index
