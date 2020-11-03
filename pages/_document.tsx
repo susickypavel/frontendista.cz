da33d6ef90447bfb1cd506fc1e3b5b7760478073
Pavel Sušický <pavels.sus@gmail.com>
2020-11-03 13:58:54 +0100

    feat: normalize.css

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import type { DocumentContext, DocumentInitialProps } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import type { NextPage } from "next";
 
 import { PageLayout } from "src/components/page-layout/page-layout.component";
 
+import "normalize.css";
+
 interface IndexProps {}
 
 const Index: NextPage<IndexProps> = () => {
