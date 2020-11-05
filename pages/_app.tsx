e251a86f6025a0a02f1f1953b7bdf081086bf172
Pavel Sušický <pavels.sus@gmail.com>
2020-11-05 16:53:58 +0100

    feat: next seo config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,32 @@
 import React, { Fragment } from "react";
 
+import { useRouter } from "next/router";
+import { DefaultSeo } from "next-seo";
+
 import type { FC } from "react";
 import type { AppProps } from "next/app";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  const { asPath } = useRouter();
+
   return (
     <Fragment>
+      <DefaultSeo
+        canonical={"https://pavelsusicky.com" + asPath}
+        openGraph={{
+          url: "https://pavelsusicky.com" + asPath,
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+          handle: "@thesoreon",
+        }}
+        additionalMetaTags={[
+          {
+            property: "author",
+            content: "Pavel Susicky",
+          },
+        ]}
+      />
       <Component {...pageProps} />
     </Fragment>
   );
--- src/components/page-layout/page-layout.component.tsx
+++ src/components/page-layout/page-layout.component.tsx
@@ -1,7 +1,43 @@
 import React, { Fragment } from "react";
 
-interface PageLayoutProps {}
+import { NextSeo } from "next-seo";
 
-export const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
-  return <Fragment>{children}</Fragment>;
+type PageLayoutProps = WritableNextSeoProps;
+
+interface WritableNextSeoProps {
+  title?: string;
+  description?: string;
+  image?: ImageMeta;
+}
+
+interface ImageMeta {
+  url: string;
+  alt: string;
+  width?: number;
+  height?: number;
+}
+
+export const PageLayout: React.FC<PageLayoutProps> = ({
+  children,
+  image = {
+    url: "https://pavelsusicky.com/temp.jpg",
+    alt: "Screenshot of pavelsusicky.com main page",
+  },
+  title = "Pavel Susicky",
+  description = "Pavel Susicky is a software engineer from Czech republic",
+}) => {
+  return (
+    <Fragment>
+      <NextSeo
+        title={title}
+        description={description}
+        openGraph={{
+          title,
+          description,
+          images: [image],
+        }}
+      />
+      {children}
+    </Fragment>
+  );
 };
