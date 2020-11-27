f6c435037960893282c685f72022275fc8405aaa
Pavel Sušický <pavels.sus@gmail.com>
2020-11-27 17:00:58 +0100

    wipe once again

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,15 @@
 import React from "react";
 
-import Link from "next/link";
-
 import type { NextPage } from "next";
 
 import { PageLayout } from "src/components/page-layout/page-layout.component";
-import {
-  heading,
-  quick_links_list,
-  sub_heading,
-} from "src/assets/stylesheets/index.styles";
 
 interface IndexProps {}
-
-const quickLinks = [
-  {
-    text: "What am I building?",
-    href: "",
-  },
-  {
-    text: "What is my environment setup?",
-    href: "",
-  },
-  {
-    text: "Which technologies do I use?",
-    href: "",
-  },
-  {
-    text: "Feel free to contact me here",
-    href: "",
-  },
-];
 
 const Index: NextPage<IndexProps> = () => {
   return (
     <PageLayout>
-      <header>
-        <h1 css={heading}>PAVEL SUSICKY</h1>
-        <h2 css={sub_heading}>Software engineer from Prague, Czech republic</h2>
-      </header>
-      <main>
-        <ul css={quick_links_list}>
-          {quickLinks.map(({ href, text }) => {
-            return (
-              <li key={text}>
-                <Link href={href}>{text}</Link>
-              </li>
-            );
-          })}
-        </ul>
-      </main>
+      <p>WIP Index Page</p>
     </PageLayout>
   );
 };
--- src/styles/global-styles.tsx
+++ src/styles/global-styles.tsx
@@ -8,33 +8,4 @@ export const GlobalStyles = css`
   :root {
     font-size: 62.5%;
   }
-
-  body {
-    font-family: "ChakraPetch", sans-serif;
-    padding: 6.4rem;
-  }
-
-  @font-face {
-    font-family: "ChakraPetch";
-    font-style: normal;
-    font-weight: 700;
-    src: local("ChakraPetch"), url("/fonts/ChakraPetch-Bold.woff2"),
-      url("/fonts/ChakraPetch-Bold.woff");
-  }
-
-  @font-face {
-    font-family: "ChakraPetch";
-    font-style: normal;
-    font-weight: 500;
-    src: local("ChakraPetch"), url("/fonts/ChakraPetch-Medium.woff2"),
-      url("/fonts/ChakraPetch-Medium.woff");
-  }
-
-  @font-face {
-    font-family: "ChakraPetch";
-    font-style: normal;
-    font-weight: 400;
-    src: local("ChakraPetch"), url("/fonts/ChakraPetch-Regular.woff2"),
-      url("/fonts/ChakraPetch-Regular.woff");
-  }
 `;
