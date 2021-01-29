99d93dcadb5369f7768872d8399e074339afe092
Pavel Sušický <pavels.sus@gmail.com>
2021-01-29 20:11:17 +0100

    feat: favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,11 @@ class MyDocument extends Document {
           <link rel="preload" href="/fonts/Inter-Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
           <link rel="preload" href="/fonts/Inter-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
           <link rel="preload" href="/fonts/Inter-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          {/* NOTE: Bitch basic favicon support, should rework later */}
+          <link rel="icon" type="image/png" href="/favicon/16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="/favicon/32.png" sizes="32x32" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="mask-icon" href="/favicon/mask-icon.svg" color="#1d4ed8" />
         </Head>
         <body>
           <Main />
