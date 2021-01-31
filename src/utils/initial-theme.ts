7f191a6150a6391ee76154a40cdf8937fbd44bb5
Pavel Sušický <pavels.sus@gmail.com>
2021-01-31 21:41:05 +0100

    test: add tests for initial theme function

--- src/utils/initial-theme.ts
+++ src/utils/initial-theme.ts
@@ -1,19 +1,13 @@
 export function getInitialColorTheme(): void {
   let theme;
+
   const persistedColorTheme = window.localStorage.getItem("theme");
 
   if (typeof persistedColorTheme === "string") {
     theme = persistedColorTheme;
-  }
-
-  const systemPreference = window.matchMedia("(prefers-color-scheme: dark)");
-
-  if (!theme) {
-    if (systemPreference.matches) {
-      theme = systemPreference.matches ? "dark" : "light";
-    } else {
-      theme = "light";
-    }
+  } else {
+    const systemPreference = window.matchMedia("(prefers-color-scheme: dark)");
+    theme = systemPreference.matches ? "dark" : "light";
   }
 
   document.documentElement.classList.add(theme);
--- test/setup.ts
+++ test/setup.ts
@@ -1,3 +1,4 @@
 import "@testing-library/jest-dom";
+import "jest-localstorage-mock";
 
 process.env.NEXT_PUBLIC_PROJECT_ID = "test-sanity-project-id";
