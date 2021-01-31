36250b74d00b9aa096445c3b4af8fb6bd20c44ef
Pavel Sušický <pavels.sus@gmail.com>
2021-01-31 10:45:44 +0100

    feat: dns-prefetch for sanity. io

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,9 +17,11 @@ class MyDocument extends Document {
           <link rel="preload" href="/fonts/Inter-Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
           <link rel="preload" href="/fonts/Inter-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
           <link rel="preload" href="/fonts/Inter-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link rel="dns-prefetch" href="https://cdn.sanity.io" />
+          <link rel="preconnect" href="https://cdn.sanity.io" />
           <link rel="manifest" href="/manifest.webmanifest" />
-          <link rel="icon" href="/favicon.ico" />
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         </Head>
         <body>
