ec14ea25e02275e3b3a361f9430fd5a846975a6e
Pavel Sušický <pavels.sus@gmail.com>
2021-02-04 23:08:18 +0100

    refactor: styles in blogfeed component

--- src/components/blogfeed-preview/blogfeed-preview.component.tsx
+++ src/components/blogfeed-preview/blogfeed-preview.component.tsx
@@ -15,13 +15,7 @@ interface BlogFeedPreviewProps {
 export const BlogFeedPreview: React.FC<BlogFeedPreviewProps> = ({ preview }) => {
   switch (preview._type) {
     case "post":
-      return (
-        <PreviewBarebone preview={preview}>
-          <span className="ml-4 font-medium text-blue-600 order-2 whitespace-nowrap">
-            {preview.categories.join(", ")}
-          </span>
-        </PreviewBarebone>
-      );
+      return <PreviewBarebone preview={preview} />;
     case "gallery":
       return <PreviewBarebone preview={preview} />;
     default:
@@ -29,17 +23,21 @@ export const BlogFeedPreview: React.FC<BlogFeedPreviewProps> = ({ preview }) => 
   }
 };
 
-const PreviewBarebone: React.FC<BlogFeedPreviewProps> = ({ children, preview }) => {
+const PreviewBarebone: React.FC<BlogFeedPreviewProps> = ({
+  children,
+  preview: { _type, slug, title, publishedAt },
+}) => {
+  const href = createDocumentPath(_type, slug);
+  const formattedDate = dateFormat(publishedAt);
+
   return (
-    <article className={classNames("monospace", styles.contentHolder)}>
-      <Link href={createDocumentPath(preview._type, preview.slug)}>
-        <a className="flex hover:underline lg:flex-col lg:items-center">
-          <h2 className={styles.heading}>{preview.title}</h2>
-          <time className="order-1 whitespace-nowrap" dateTime={preview.publishedAt}>
-            {dateFormat(preview.publishedAt)}
-          </time>
-          <div className={classNames("flex items-center order-4", styles.badgeHolder)}>
-            <Badge type={preview._type} />
+    <article className={classNames("monospace", styles.preview)}>
+      <Link href={href}>
+        <a className={styles.preview__link}>
+          <h2>{title}</h2>
+          <time dateTime={publishedAt}>{formattedDate}</time>
+          <div className={styles.preview__badgeholder}>
+            <Badge type={_type} />
           </div>
           {children}
         </a>
