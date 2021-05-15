248d065b9e3d343e67e8b4cc9e1c140eac0de041
Pavel Sušický <pavels.sus@gmail.com>
2021-05-15 20:07:57 +0200

    feat: links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Link from "next/link";
 
 import type { NextPage } from "next";
 
-import styles from "src/assets/index.module.scss";
-
 import { PageLayout } from "src/components/page-layout/page-layout.component";
-import { Card } from "src/components/card/card.component";
+
+import styles from "src/assets/index.module.scss";
 
 const IndexPage: NextPage = () => {
   return (
@@ -14,21 +12,26 @@ const IndexPage: NextPage = () => {
       title="home"
       wrapper={{
         element: "main",
-        classes: "p-4",
+        classes: "p-4 flex flex-col space-y-4",
       }}
     >
-      <Card title="About me">
-        <ul className={"text-white mb-4 relative pl-4 " + styles["card-list"]}>
-          <li>Software engineer student</li>
-          <li>Calisthenics</li>
-          <li>Computer games</li>
-        </ul>
-        <Link href="/about">
-          <a className="inline-block bg-blue-500 hover:bg-blue-600 w-full p-2 rounded text-center font-bold">
-            Show me more!
-          </a>
-        </Link>
-      </Card>
+      <h1 className="text-4xl font-bold">Pavel Susicky</h1>
+      <p className="text-lg">
+        Student of{" "}
+        <strong className={styles["description-school"]}>
+          Czech Technical University in Prague
+        </strong>
+        , aspiring software engineer.
+      </p>
+
+      <ul className={styles["social-list"]}>
+        <li className={styles["linkedIn"]}>
+          <a href="https://www.linkedin.com/in/pavel-susicky/">LinkedIn</a>
+        </li>
+        <li className={styles["github"]}>
+          <a href="https://github.com/Thesoreon">GitHub</a>
+        </li>
+      </ul>
     </PageLayout>
   );
 };
