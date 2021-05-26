f5904aaece4561a6165fe8f71777200cc5686623
Pavel Sušický <pavels.sus@gmail.com>
2021-05-26 19:28:56 +0200

    feat: Quicksand font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment, FunctionComponent } from "react";
+import Head from "next/head";
 import { DefaultSeo } from "next-seo";
 
 import type { AppProps } from "next/app";
@@ -16,6 +17,36 @@ const App: FunctionComponent<AppProps> = ({
 
   return (
     <Fragment>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/Quicksand-Bold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Quicksand-SemiBold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Quicksand-Medium.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Quicksand-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <DefaultSeo
         titleTemplate="pavelsusicky.com | %s"
         defaultTitle="pavelsusicky.com"
--- src/components/common/components/button/button.d.ts
+++ src/components/common/components/button/button.d.ts
@@ -1,3 +1,35 @@
+type ColorsWithoutVariants = "black" | "white";
+
+type ButtonColors =
+  | "gray"
+  | "red"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "indigo"
+  | "purple"
+  | "pink";
+
+type ButtonColorsVariant =
+  | 50
+  | 100
+  | 200
+  | 300
+  | 400
+  | 500
+  | 600
+  | 700
+  | 800
+  | 900;
+
+type ForegroundColors =
+  | `text_${ButtonColors}_${ButtonColorsVariant}`
+  | `text_${ColorsWithoutVariants}`;
+
+type BackgroundColors =
+  | `bg_${ButtonColors}_${ButtonColorsVariant}`
+  | `bg_${ColorsWithoutVariants}`;
+
 export interface ButtonProps {
   className?: string;
 }
