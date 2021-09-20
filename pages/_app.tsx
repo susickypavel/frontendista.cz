4188a6f5d838e463012bfbeb9a682fd7637aaf5f
Pavel Sušický <susicky.pavel@outlook.cz>
2021-09-20 22:56:37 +0200

    feat(seo): add seo config barebone

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
+import { useRouter } from "next/router";
+import { DefaultSeo } from "next-seo";
 
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const { asPath } = useRouter();
+
+  return (
+    <React.Fragment>
+      <DefaultSeo canonical={process.env.NEXT_PUBLIC_DOMAIN + asPath} />
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 };
 
 export default App;
---
+++ src/components/layout.component.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { NextSeo } from "next-seo";
+
+interface LayoutProps {
+  title?: string;
+}
+
+export const Layout: React.FunctionComponent<LayoutProps> = ({
+  children,
+  title = "Pavel Susicky",
+}) => {
+  return (
+    <React.Fragment>
+      <NextSeo title={title} />
+      {children}
+    </React.Fragment>
+  );
+};
