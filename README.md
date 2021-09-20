ab65001a96541d18922a24372d141a157dc3afac
Pavel Sušický <susicky.pavel@outlook.cz>
2021-09-20 22:32:26 +0200

    feat(cms): sanity blog content

---
+++ README.md
@@ -0,0 +1,7 @@
+## Environment variables
+
+| Name                | Value       | Required |
+| ------------------- | ----------- | -------- |
+| SANITY_DATASET_ID   | development | ✅       |
+| SANITY_PROJECT_ID   | _hidden_    | ✅       |
+| SANITY_CLIENT_TOKEN | _hidden_    | ✅       |
---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import BlockContent from "@sanity/block-content-to-react";
+
+import { fetchOrThrow } from "../../src/utils/sanity-client-utils";
+
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
+
+const BlogPost: NextPage<any> = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <p>{props.publishedAt}</p>
+      <BlockContent blocks={props.body} serializers={{}} />
+    </div>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  try {
+    const postsQuery = `*[_type == "post"] {
+      "slug": slug.current
+    }`;
+
+    const result = await fetchOrThrow<{ slug: string }[]>(postsQuery);
+    const paths = result.map((post) => `/blog/${post.slug}`);
+
+    return {
+      paths,
+      fallback: false,
+    };
+  } catch (error) {
+    console.info(error);
+
+    if (process.env.NODE_ENV === "production") {
+      process.exit(1);
+    }
+
+    throw new Error("[SSG][BLOG]: An error occured in getStaticPaths.");
+  }
+};
+
+export const getStaticProps: GetStaticProps<any, { slug: string }> = async (
+  ctx
+) => {
+  try {
+    const postQuery = `*[_type == "post" && slug.current == $slug] {
+      ...
+    }[0]`;
+
+    const result = await fetchOrThrow(postQuery, {
+      slug: ctx.params?.slug,
+    });
+
+    return {
+      props: result,
+    };
+  } catch (error) {
+    console.info(error);
+
+    if (process.env.NODE_ENV === "production") {
+      process.exit(1);
+    }
+
+    throw new Error("[SSG][BLOG]: An error occured in getStaticProps.");
+  }
+};
+
+export default BlogPost;
