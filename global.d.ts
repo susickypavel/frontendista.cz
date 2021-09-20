e265d4d7c881796695f4c37c1f3d6dfd89fe1575
Pavel Sušický <susicky.pavel@outlook.cz>
2021-09-20 22:32:26 +0200

    feat(cms): sanity blog content

---
+++ global.d.ts
@@ -0,0 +1,8 @@
+declare module "@sanity/block-content-to-react" {
+  let component: React.FunctionComponent<{
+    serializers: any;
+    blocks: any;
+  }>;
+
+  export default component;
+}
---
+++ src/utils/sanity-client-utils.ts
@@ -0,0 +1,28 @@
+import SanityClient from "@sanity/client";
+
+function getSanityClient() {
+  const sanityClient = SanityClient({
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_DATASET_ID ?? "development",
+    token: process.env.SANITY_CLIENT_TOKEN,
+    apiVersion: "2021-09-12",
+    useCdn: false,
+  });
+
+  return sanityClient;
+}
+
+export async function fetchOrThrow<T = Array<any>, V extends Object = {}>(
+  query: string,
+  variables?: V
+): Promise<T> {
+  const client = getSanityClient();
+
+  const result = await client.fetch<T>(query, variables ?? {});
+
+  if (!result || (Array.isArray(result) && result.length === 0)) {
+    throw new Error(`[SANITY][NOCDN]: Result wasn't defined or empty.`);
+  }
+
+  return result;
+}
