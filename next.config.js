9b399182560b536635b0e0bf345083b59b807be6
Pavel Sušický <susicky.pavel@outlook.cz>
2021-09-20 23:13:44 +0200

    chore(config): add environment variable check

--- next.config.js
+++ next.config.js
@@ -1,3 +1,40 @@
-module.exports = {
+const { default: chalk } = require("chalk");
+
+(() => {
+  function checkEnvironmentVariables() {
+    const requiredVariables = [
+      "NEXT_PUBLIC_DOMAIN",
+      "SANITY_PROJECT_ID",
+      "SANITY_DATASET_ID",
+      "SANITY_CLIENT_TOKEN",
+    ];
+
+    let isOkFlag = true;
+
+    requiredVariables.forEach((variable) => {
+      if (!process.env[variable]) {
+        console.log(
+          chalk.red(
+            `'${variable}' is a required environment variable and it's not set.`
+          )
+        );
+        isOkFlag = false;
+      }
+    });
+
+    if (!isOkFlag && process.env.NODE_ENV === "production") {
+      process.exit(1);
+    }
+  }
+
+  checkEnvironmentVariables();
+})();
+
+/**
+ * @type {import("next").NextConfig}
+ */
+const nextConfig = {
   reactStrictMode: true,
 };
+
+module.exports = nextConfig;
