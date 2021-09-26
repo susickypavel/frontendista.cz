ca32f10a5f7dddde3c78c04243fa385168513140
Pavel Sušický <susicky.pavel@outlook.cz>
2021-09-26 14:43:57 +0200

    feat(graphql): migrate blog API fetching to graphql

--- src/utils/apollo-client-utils.ts
+++ src/utils/apollo-client-utils.ts
@@ -1,26 +1,33 @@
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-export function getApolloClient() {
+import type { DocumentNode } from "graphql";
+
+function getApolloClient() {
   const client = new ApolloClient({
     uri: process.env.SANITY_GRAPHQL_ENDPOINT,
     cache: new InMemoryCache(),
+    headers: {
+      Authorization: `Bearer ${process.env.SANITY_CLIENT_TOKEN}`,
+    },
   });
 
   return client;
 }
 
-export async function fetchOrThrow() {
-  const client = getApolloClient();
-
-  const { data } = await client.query({
-    query: gql`
-      query {
-        allPost {
-          slug {
-            current
-          }
-        }
-      }
-    `,
+export async function fetchOrThrow<Q, V = {}>(
+  query: DocumentNode,
+  variables?: V,
+  client = getApolloClient()
+) {
+  const { data, error, errors } = await client.query<Q, V>({
+    query,
+    variables,
+    fetchPolicy: "network-only",
   });
+
+  if (error || errors) {
+    throw error || errors;
+  }
+
+  return data;
 }
