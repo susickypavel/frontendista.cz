56464688738507ac5bfaa8f8f0c85b6e41e437c2
Pavel Sušický <susicky.pavel@outlook.cz>
2022-01-20 18:29:00 +0100

    fix: remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,10 @@ import type { NextPage } from "next";
 
 import { Button } from "@components/common/button";
 
-import {
-  CogIcon,
-  SunIcon,
-  PaperAirplaneIcon,
-  ThumbUpIcon,
-  BanIcon,
-} from "@heroicons/react/solid";
-
 const Home: NextPage = () => {
   return (
     <div className="bg-gray-200 p-4 space-x-4 inline-block rounded-lg">
-      <Button size="base" icon={SunIcon} />
-      <Button size="base" icon={CogIcon} />
-      <Button size="base" icon={PaperAirplaneIcon} />
-      <Button size="base" icon={BanIcon} isDisabled />
-      <Button size="base" icon={ThumbUpIcon} />
+      <Button>Ahoj</Button>
     </div>
   );
 };
