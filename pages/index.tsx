033a1ace2c76dab5741c848b85edf081ecbc1f98
Pavel Sušický <susicky.pavel@outlook.cz>
2022-02-12 14:14:35 +0100

    chore(css): add scss support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 
+import styles from "../src/stylesheet/pages/index.module.scss";
+
 import type { NextPage } from "next";
 
 const IndexPage: NextPage = () => {
   return (
-    <div>
+    <div className={styles.page}>
       <h1>Hello Next.js</h1>
     </div>
   );
