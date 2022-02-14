e551c0d3fd14b7eeb09993b9921ceaad6b9e629e
Pavel Sušický <susicky.pavel@outlook.cz>
2022-02-14 23:14:08 +0100

    chore(lint): add stylelint

---
+++ stylelint.config.cjs
@@ -0,0 +1,9 @@
+/**
+ * @type {import("stylelint").Config}
+ */
+const config = {
+  extends: ["stylelint-config-standard-scss", "stylelint-config-prettier-scss"],
+  rules: [],
+};
+
+module.exports = config;
