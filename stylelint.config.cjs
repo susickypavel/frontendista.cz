6bda207512288aa33707833fba76998145cd42d1
Pavel Sušický <susicky.pavel@outlook.cz>
2022-02-15 22:40:42 +0100

    chore(stylelint): add plugins and recommended configs

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -2,8 +2,17 @@
  * @type {import("stylelint").Config}
  */
 const config = {
-  extends: ["stylelint-config-standard-scss", "stylelint-config-prettier-scss"],
-  rules: [],
+  extends: [
+    "stylelint-config-standard-scss",
+    "stylelint-config-prettier-scss",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-idiomatic-order",
+  ],
+  plugins: ["stylelint-order"],
+  rules: {
+    // CamelCase pattern
+    "selector-class-pattern": "^[a-z][a-zA-Z0-9]+$",
+  },
 };
 
 module.exports = config;
