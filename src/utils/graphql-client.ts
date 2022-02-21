84a987c6b997c9e5a6bd4d33a04f7cbf943274ed
Pavel Sušický <susicky.pavel@outlook.cz>
2022-02-21 19:49:19 +0100

    feat(graphql): add client with credentials

---
+++ src/utils/graphql-client.ts
@@ -0,0 +1,7 @@
+import { GraphQLClient } from "graphql-request";
+
+export const GRAPHQL_CLIENT = new GraphQLClient(process.env.SANITY_GRAPHQL_ENDPOINT, {
+  headers: {
+    authorization: `Bearer ${process.env.SANITY_CLIENT_TOKEN}`,
+  },
+});
