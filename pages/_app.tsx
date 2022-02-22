11c816c8487a73dbddeb1fe25c4fa8e80deda599
Pavel Sušický <susicky.pavel@outlook.cz>
2022-02-22 18:51:56 +0100

    feat(canonical): add self-canonical URL to app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
+import * as React from "react";
+
+import { useRouter } from "next/router";
+import { DefaultSeo } from "next-seo";
+
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 
 const Application: NextPage<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  return (
+    <React.Fragment>
+      <DefaultSeo
+        defaultTitle="Pavel Susicky - Software engineer"
+        description="Front-end developer, aspiring software engineer."
+        // NOTE: This strips all query params from the URL which might not be desired.
+        // Imagine that you have a page with query param that changes the content of the page.
+        // This may result in confusion for a web crawler.
+        canonical={"https://frontendista.cz" + router.asPath.split("?")[0]}
+      />
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 };
 
 export default Application;
