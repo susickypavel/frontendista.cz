3b53d87fe0da9b88bdf29decd08c7993cb55c72b
Pavel Sušický <susicky.pavel@outlook.cz>
2022-02-25 21:29:10 +0100

    feat(optimization): add preact to production bundle

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@
  */
 const config = {
   reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.resolve.alias["react-dom"] = "preact/compat";
+      config.resolve.alias["react"] = "preact/compat";
+    }
+
+    return config;
+  },
 };
 
 export default config;
