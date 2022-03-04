eccbedc6855065011e8d80cd70eebca5c5ebd216
Pavel Sušický <susicky.pavel@outlook.cz>
2022-03-04 13:03:27 +0100

    chore(env): add env validation function
    Closes #22

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,34 @@
+import chalk from "chalk";
+
+function validateEnvVars() {
+  const requiredVars = [
+    "SANITY_GRAPHQL_ENDPOINT",
+    "SANITY_CLIENT_TOKEN",
+    "SANITY_PROJECT_ID",
+    "SANITY_DATASET_ID",
+  ];
+
+  requiredVars.reduce((hasFailed, variable) => {
+    const isMissing = !process.env[variable];
+
+    if (isMissing) {
+      console.error(
+        `${chalk.red`env  `} - ${chalk.bold.redBright`${variable}`} is required`,
+      );
+    }
+
+    return hasFailed || isMissing;
+  }, false) && process.exit(1);
+}
+
 /**
  * @type {import("next").NextConfig}
  */
 const config = {
   reactStrictMode: true,
   webpack: (config, { dev, isServer }) => {
+    validateEnvVars();
+
     if (!dev && !isServer) {
       config.resolve.alias["react-dom"] = "preact/compat";
       config.resolve.alias["react"] = "preact/compat";
