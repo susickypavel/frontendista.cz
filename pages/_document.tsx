c6845397e5c6986f3401e94877767620c4ca2655
Pavel Sušický <susicky.pavel@outlook.cz>
2022-03-08 11:18:20 +0100

    feat(font): add font-face declarations into head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,13 @@ import type { NextPage } from "next";
 const Document: NextPage = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `@font-face{font-family:QS;src:url("/fonts/QS/700.woff2") format("woff2"),url("/fonts/QS/700.woff") format("woff");font-display:swap;font-weight:700}@font-face{font-family:QS;src:url("/fonts/QS/600.woff2") format("woff2"),url("/fonts/QS/600.woff") format("woff");font-display:swap;font-weight:600}@font-face{font-family:QS;src:url("/fonts/QS/500.woff2") format("woff2"),url("/fonts/QS/500.woff") format("woff");font-display:swap;font-weight:500}@font-face{font-family:QS;src:url("/fonts/QS/400.woff2") format("woff2"),url("/fonts/QS/400.woff") format("woff");font-display:swap;font-weight:400}@font-face{font-family:QS;src:url("/fonts/QS/300.woff2") format("woff2"),url("/fonts/QS/300.woff") format("woff");font-display:swap;font-weight:300}`,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
