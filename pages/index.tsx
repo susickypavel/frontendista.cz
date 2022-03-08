575da52662b2266de1be3031befe80ea7c8c3a46
Pavel Sušický <susicky.pavel@outlook.cz>
2022-03-08 11:18:20 +0100

    feat(font): add font-face declarations into head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 
+import styles from "@stylesheets/pages/index.module.scss";
+
 import type { NextPage } from "next";
 
 const IndexPage: NextPage = () => {
-  return <div>IndexPage</div>;
+  return <div className={styles.page}>IndexPage</div>;
 };
 
 export default IndexPage;
