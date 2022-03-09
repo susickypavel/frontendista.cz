f70c15f56976080e9ba0a0db4f7b2c6dd11e45a1
Pavel Sušický <susicky.pavel@outlook.cz>
2022-03-10 00:02:16 +0100

    feat(button): add useButtonContent hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,16 @@ import styles from "@stylesheets/pages/index.module.scss";
 
 import type { NextPage } from "next";
 
+import { ToggleButton } from "@components/common/button/toggle-button/toggle-button.component";
+import { Button } from "@components/common/button/button/button.component";
+
 const IndexPage: NextPage = () => {
-  return <div className={styles.page}>IndexPage</div>;
+  return (
+    <div className={styles.page}>
+      <Button>Button</Button>
+      <ToggleButton>ToggleButton</ToggleButton>
+    </div>
+  );
 };
 
 export default IndexPage;
---
+++ src/utils/hooks/useButtonContent.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+import type { IToggleButtonProps } from "@components/common/button/toggle-button/toggle-button.component";
+
+/**
+ * NOTE: There is optional React.useMemo for "optimization" for future reference, if there is a performance bottleneck.
+ */
+export function useButtonContent({
+  icon: Icon,
+  icons: Icons,
+  children,
+}: Pick<IToggleButtonProps, "icons" | "icon" | "children">) {
+  // return React.useMemo(() => {
+  if (Icon) {
+    return <Icon />;
+  }
+
+  return (
+    <React.Fragment>
+      {Icons?.left && <Icons.left />}
+      {children}
+      {Icons?.right && <Icons.right />}
+    </React.Fragment>
+  );
+  // }, [children, Icon, Icons]);
+}
