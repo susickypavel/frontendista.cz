cacc24488ebeac4495e31165dcb5f2b729b8fc9c
Pavel Sušický <susicky.pavel@outlook.cz>
2022-03-10 18:32:30 +0100

    chore(tailwind): add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import("tailwindcss/tailwind-config").TailwindConfig}
+ */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
