0ea62eef282a7edeb41e9af1c8234f6505ce288f
Pavel Sušický <susicky.pavel@outlook.cz>
2022-03-11 12:29:19 +0100

    fix(button): pressed styles are applied when toggle is selected

--- src/utils/hooks/useButtonStyle.tsx
+++ src/utils/hooks/useButtonStyle.tsx
@@ -31,11 +31,10 @@ export function useButtonStyle({
     "inline-flex justify-center items-center bg-white text-black rounded-lg w-8 appearance-none outline-none",
     buttonSizes[size],
     {
+      "bg-red-500": isSelected && !isPressed,
       "bg-slate-500": isPressed,
-      "bg-red-500": isSelected,
       "ring-2 ring-white ring-offset-2 ring-offset-current transition-shadow duration-200":
-        isFocused && isFocusVisible,
-      "styles.isHovered": isHovered,
+        (isFocused && isFocusVisible) || isHovered,
     },
     className,
   );
