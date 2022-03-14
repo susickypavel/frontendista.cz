17a8098f15e0c1ae98fcba10d71959e15fb25965
Pavel Sušický <susicky.pavel@outlook.cz>
2022-03-14 08:47:45 +0100

    chore(tailwind): add max-width media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+    },
     extend: {
       fontFamily: {
         sans: ["QS", ...fontFamily.sans],
