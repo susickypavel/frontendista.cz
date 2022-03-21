dc499e150f329fdf0e231b5c3a542a8b32b5ec38
Pavel Sušický <susicky.pavel@outlook.cz>
2022-03-21 09:31:57 +0100

    refactor(clean): remove old components

--- src/components/context-actions/context-actions.component.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-import styles from "./context-actions.module.scss";
-
-export const ContextActions: React.FunctionComponent = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
-};
--- src/components/top-bar/top-bar.component.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-import styles from "./top-bar.module.scss";
-
-export const TopBar: React.FunctionComponent = ({ children }) => {
-  return <div className={styles.topbar}>{children}</div>;
-};
