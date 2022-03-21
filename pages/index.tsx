16ba42711b0b6d3d2100a5ad3288e138f95654a5
Pavel Sušický <susicky.pavel@outlook.cz>
2022-03-21 09:23:10 +0100

    feat(animation): add mount animations to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,48 @@
 import * as React from "react";
+import { LazyMotion, m, domAnimation } from "framer-motion";
 
 import styles from "@stylesheets/pages/index-page.module.scss";
 
 import type { NextPage } from "next";
+import type { Variants } from "framer-motion";
+
+const animationRoot: Variants = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.5, staggerDirection: 1 },
+  },
+};
+
+const childAnimation: Variants = {
+  hidden: { x: -32, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
+};
 
 const IndexPage: NextPage = () => {
   return (
-    <React.Fragment>
-      <header className={styles.hero}>
-        <h1 className={styles.heroTitle}>Pavel Susicky</h1>
-        <p className={styles.heroSubtitle}>Software Engineer</p>
-        <p className={styles.heroSubtitle}>Front-end developer</p>
-      </header>
-    </React.Fragment>
+    <LazyMotion features={domAnimation}>
+      <m.header
+        variants={animationRoot}
+        initial="hidden"
+        animate="visible"
+        className={styles.hero}>
+        <m.h1 variants={childAnimation} className={styles.heroTitle}>
+          Pavel Susicky
+        </m.h1>
+        <m.p variants={childAnimation} className={styles.heroSubtitle}>
+          Software Engineer
+        </m.p>
+        <m.p variants={childAnimation} className={styles.heroSubtitle}>
+          Front-end developer
+        </m.p>
+        <m.p variants={childAnimation} className={styles.heroSubtitle}>
+          Prague, Czech Republic
+        </m.p>
+      </m.header>
+    </LazyMotion>
   );
 };
 
