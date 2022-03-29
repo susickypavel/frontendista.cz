a9d25247ebb8cfce7e16cfdfaa0be9bfa612cf91
Pavel Sušický <susicky.pavel@outlook.cz>
2022-03-29 15:22:56 +0200

    feat(page): add contact page file

---
+++ pages/contact.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+// import styles from "@stylesheets/pages/contact-page.module.scss";
+
+import type { NextPage } from "next";
+
+const ContactPage: NextPage = () => {
+  return <main>ContactPage</main>;
+};
+
+export default ContactPage;
