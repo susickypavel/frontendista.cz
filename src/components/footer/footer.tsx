26a17534fc33df34f2bfd566534f2fd182f42765
Pavel Sušický <susicky.pavel@outlook.cz>
2022-03-29 14:35:56 +0200

    feat(footer): add new sections and styles

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,38 @@ import styles from "./footer.module.scss";
 
 export const Footer: React.FunctionComponent = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <section>
+          <h1>Pavel Susicky</h1>
+          <p>
+            Aspiring software engineer <br /> from Czech Republic.
+          </p>
+        </section>
+        <section>
+          <h1>Follow me</h1>
+          <ul>
+            <li>GitHub</li>
+            <li>Twitter</li>
+          </ul>
+        </section>
+        <section>
+          <h1>Build with</h1>
+          <ul>
+            <li>Vercel</li>
+            <li>Sanity</li>
+          </ul>
+        </section>
+        <section>
+          <h1>Sitemap</h1>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Blog</li>
+            <li>Contact</li>
+          </ul>
+        </section>
+      </div>
       <span className={styles.copyrightNotice}>
         &copy; 2022, Pavel Sušický. All rights reserved.
       </span>
