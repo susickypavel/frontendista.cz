d94ac177d8b4b900d2f2cbe674c368ad65bf80ad
Pavel Sušický <susicky.pavel@outlook.cz>
2022-03-30 10:20:46 +0200

    feat(nav): replace old AnchorLink components

--- src/components/navigation/navigation.component.tsx
+++ src/components/navigation/navigation.component.tsx
@@ -2,57 +2,23 @@ import * as React from "react";
 
 import styles from "./navigation.module.scss";
 
-import { AnchorLink } from "@components/common/anchor-link/anchor-link.component";
-
-import { HiChevronDown } from "react-icons/hi";
+import { AnchorLink } from "@components/experiments/link";
 
 export const Navigation: React.FunctionComponent = () => {
   return (
     <nav className={styles.navigation}>
       <ul className={styles.navigationList}>
         <li>
-          <AnchorLink
-            icons={{
-              right: HiChevronDown,
-            }}
-            nextLinkProps={{
-              href: "/",
-            }}>
-            Home
-          </AnchorLink>
+          <AnchorLink href="/">Home</AnchorLink>
         </li>
         <li>
-          <AnchorLink
-            icons={{
-              right: HiChevronDown,
-            }}
-            nextLinkProps={{
-              href: "/about",
-            }}>
-            About
-          </AnchorLink>
+          <AnchorLink href="/about">About</AnchorLink>
         </li>
         <li>
-          <AnchorLink
-            icons={{
-              right: HiChevronDown,
-            }}
-            nextLinkProps={{
-              href: "/blog",
-            }}>
-            Blog
-          </AnchorLink>
+          <AnchorLink href="/blog">Blog</AnchorLink>
         </li>
         <li>
-          <AnchorLink
-            icons={{
-              right: HiChevronDown,
-            }}
-            nextLinkProps={{
-              href: "/contact",
-            }}>
-            Contact
-          </AnchorLink>
+          <AnchorLink href="/contact">Contact</AnchorLink>
         </li>
       </ul>
     </nav>
