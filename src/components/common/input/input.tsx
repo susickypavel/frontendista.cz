03a8ab2da26453b3b06079cff5016129e4ca0186
Pavel Sušický <susicky.pavel@outlook.cz>
2022-04-03 11:05:01 +0200

    feat(input): add error and focus states

--- src/components/common/input/input.tsx
+++ src/components/common/input/input.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import clsx from "clsx";
+import { mergeProps, useFocus } from "react-aria";
+import { HiExclamationCircle } from "react-icons/hi";
 
 import styles from "./input.module.scss";
 
@@ -11,25 +13,63 @@ interface IInput {
    */
   label: string;
   /**
+   * Placeholder value for the input
+   */
+  placeholder?: string;
+  /**
    * Form errors associated with the input
    */
   errors?: FieldError;
+  /**
+   * @default {}
+   */
+  domProps?: React.InputHTMLAttributes<HTMLInputElement>;
 }
 
 export const Input = React.forwardRef<HTMLInputElement, IInput>(
-  ({ errors, label, ...props }, forwardedRef) => {
+  (
+    {
+      errors,
+      label,
+      placeholder,
+      domProps = {
+        type: "text",
+      },
+      ...props
+    },
+    forwardedRef,
+  ) => {
+    const [isFocused, setFocused] = React.useState(false);
+    const { focusProps } = useFocus({
+      onFocusChange: setFocused,
+    });
+
     props.children = undefined;
 
     return (
       <label
         className={clsx(styles.label, {
           [styles.formError]: errors,
+          [styles.focused]: isFocused,
         })}>
-        {label}
+        <span className={styles.topBar}>
+          <span
+            className={clsx(styles.labelText, {
+              [styles.labelTextRequiredMark]: domProps.required,
+            })}>
+            {label}
+          </span>
+          {errors && (
+            <p className={styles.errorMessage}>
+              <HiExclamationCircle />
+              {errors.message}
+            </p>
+          )}
+        </span>
         <input
           ref={forwardedRef}
-          placeholder="Optional, fill if you want me to contact you 😉"
-          {...props}
+          placeholder={placeholder}
+          {...mergeProps(domProps, props, focusProps)}
         />
       </label>
     );
