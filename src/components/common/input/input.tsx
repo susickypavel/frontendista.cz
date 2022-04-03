5c237fdf1ff49c06a2a17fda0095305385865f75
Pavel Sušický <susicky.pavel@outlook.cz>
2022-04-03 11:47:46 +0200

    refactor(label): add label component

--- src/components/common/input/input.tsx
+++ src/components/common/input/input.tsx
@@ -1,32 +1,22 @@
 import * as React from "react";
-import clsx from "clsx";
-import { mergeProps, useFocus } from "react-aria";
-import { HiExclamationCircle } from "react-icons/hi";
+import { mergeProps } from "react-aria";
 
-import styles from "./input.module.scss";
+import { Label } from "../label/label";
 
-import type { FieldError } from "react-hook-form";
+import type { ILabelProps } from "../label/label";
 
-interface IInput {
-  /**
-   * The label for the input.
-   */
-  label: string;
+interface IInputProps extends Omit<ILabelProps, "required" | "children"> {
   /**
    * Placeholder value for the input
    */
   placeholder?: string;
-  /**
-   * Form errors associated with the input
-   */
-  errors?: FieldError;
   /**
    * @default {}
    */
   domProps?: React.InputHTMLAttributes<HTMLInputElement>;
 }
 
-export const Input = React.forwardRef<HTMLInputElement, IInput>(
+export const Input = React.forwardRef<HTMLInputElement, IInputProps>(
   (
     {
       errors,
@@ -39,39 +29,16 @@ export const Input = React.forwardRef<HTMLInputElement, IInput>(
     },
     forwardedRef,
   ) => {
-    const [isFocused, setFocused] = React.useState(false);
-    const { focusProps } = useFocus({
-      onFocusChange: setFocused,
-    });
-
-    props.children = undefined;
-
     return (
-      <label
-        className={clsx(styles.label, {
-          [styles.formError]: errors,
-          [styles.focused]: isFocused,
-        })}>
-        <span className={styles.topBar}>
-          <span
-            className={clsx(styles.labelText, {
-              [styles.labelTextRequiredMark]: domProps.required,
-            })}>
-            {label}
-          </span>
-          {errors && (
-            <p className={styles.errorMessage}>
-              <HiExclamationCircle />
-              {errors.message}
-            </p>
-          )}
-        </span>
-        <input
-          ref={forwardedRef}
-          placeholder={placeholder}
-          {...mergeProps(domProps, props, focusProps)}
-        />
-      </label>
+      <Label label={label} errors={errors} required={domProps.required}>
+        {({ focusProps }) => (
+          <input
+            ref={forwardedRef}
+            placeholder={placeholder}
+            {...mergeProps(domProps, props, focusProps)}
+          />
+        )}
+      </Label>
     );
   },
 );
