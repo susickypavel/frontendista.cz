c543afef850618d7ebaeb4e94da08c74d5f5e337
Pavel Sušický <susicky.pavel@outlook.cz>
2022-04-10 22:24:05 +0200

    feat(embed): add codesandbox embed component

---
+++ src/components/codesandbox-embed/codesandbox-embed.d.ts
@@ -0,0 +1,10 @@
+export interface ICodeSandboxEmbedProps {
+  /**
+   * SandBox id retrieved through CMS.
+   */
+  sandboxId: string;
+  /**
+   * Title describing the CodeSandbox.
+   */
+  title: string;
+}
---
+++ src/components/codesandbox-embed/index.ts
@@ -0,0 +1,1 @@
+export { CodeSandboxEmbed } from "./codesandbox-embed";
