7e8a59edb5be1e5b05d47eb01527a4613dbea007
Pavel Sušický <susicky.pavel@outlook.cz>
2022-04-12 09:13:11 +0200

    feat(image): add image with caption to block content

--- src/components/blog-image/blog-image.component.tsx
+++ src/components/blog-image/blog-image.component.tsx
@@ -7,21 +7,30 @@ export interface IBlogImageProps {
   src: string;
   lqip: string;
   aspectRatio?: number;
+  alt: string;
+  caption?: string;
 }
 
 export const BlogImage: React.FunctionComponent<IBlogImageProps> = ({
   lqip,
   // eslint-disable-next-line no-unused-vars
   aspectRatio,
+  caption,
+  alt,
   ...props
 }) => {
+  let Wrapper = caption ? "figure" : React.Fragment;
+
   return (
-    <Image
-      placeholder="blur"
-      layout="responsive"
-      blurDataURL={lqip}
-      alt="TODO"
-      {...props}
-    />
+    <Wrapper>
+      <Image
+        placeholder="blur"
+        layout="responsive"
+        blurDataURL={lqip}
+        alt={alt}
+        {...props}
+      />
+      {caption && <figcaption>{caption}</figcaption>}
+    </Wrapper>
   );
 };
